2e3ede22132a65eacd6abd4b808a4b47cf48df46
subhanjamil652 <Subhanjamil652@gmail.com>
2026-04-18 08:32:08 +0100

    feat(rails): performance page with live model metrics

--- log_anomaly_rails/app/controllers/performance_controller.rb
+++ log_anomaly_rails/app/controllers/performance_controller.rb
@@ -2,6 +2,7 @@ class PerformanceController < ApplicationController
   def index
     @models_resp = MlApiService.get_models
     @models      = @models_resp["models"] || []
+    @metrics_source = @models_resp["metrics_source"]
     @active      = @models.find { |m| m["is_active"] } || @models.first
     @trained_at  = @models_resp["trained_at"]
 
@@ -15,7 +16,7 @@ class PerformanceController < ApplicationController
       # Approximate ROC curve from AUC using a beta distribution curve
       fpr_pts = (0..20).map { |i| i / 20.0 }
       tpr_pts = fpr_pts.map do |fpr|
-        [[(fpr ** (1.0 / [auc * 2, 0.5].max)).round(3), 0.0].max, 1.0].min
+        [ [ (fpr ** (1.0 / [ auc * 2, 0.5 ].max)).round(3), 0.0 ].max, 1.0 ].min
       end
       { name: name, fpr: fpr_pts, tpr: tpr_pts, auc: auc }
     end
@@ -29,7 +30,7 @@ class PerformanceController < ApplicationController
       normal:   4_399_503,
       imbalance_ratio: "1:12.6",
       window_size: 20,
-      stride: 10,
+      stride: 10
     }
   end
 end
